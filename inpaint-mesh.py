683f3b6489977c9111a0c528900a015e8fb5c705
Baole Fang <baole.fang@gmail.com>
2024-01-24 12:58:09 -0500

    feat: inpaint under 3 cases

--- inpaint-mesh.py
+++ inpaint-mesh.py
@@ -9,28 +9,15 @@ from PIL import Image, ImageChops
 
 
 def generate_data():
-    img = np.array(init_image)
-    n_masks = np.random.randint(*ms)
-    mask = np.zeros(img.shape, dtype=np.uint8)
-    for i in range(n_masks):
-        h = np.random.randint(*ws)
-        w = np.random.randint(*hs)
-        x = np.random.randint(init_image.height // 2, init_image.height - h)
-        y = np.random.randint(0, init_image.width - w)
-        mask[x:x + h, y:y + w, :] = 255
-        img[x:x + h, y:y + w, :] = 0
-    mask = Image.fromarray(mask)
-    img = Image.fromarray(img)
     return img, mask
 
 
-root = "/data/ykwon/data/THuman2.0/val"
-output = "val"
+root = "val"
+output = "val-inpaint"
 ws = (50, 200)
 hs = (50, 200)
 ms = (3, 5)
 shape = (512, 512)
-batch_size = 1
 
 pipeline = AutoPipelineForInpainting.from_pretrained(
     "runwayml/stable-diffusion-inpainting",
@@ -50,15 +37,21 @@ n = len(os.listdir(root))
 for i, subject in enumerate(os.listdir(root)):
     print(f'Processing {subject} ({i + 1}/{n}) ...')
     init_image = load_image(os.path.join(root, subject, "material0.jpeg")).resize(shape)
-    imgs, masks, prompts = [], [], []
-    for _ in range(batch_size):
-        img, mask = generate_data()
-        imgs.append(img)
-        masks.append(mask)
-        prompts.append(prompt)
-    images = pipeline(prompt=prompts, image=imgs, mask_image=masks, height=shape[0], width=shape[1],
-                      generator=generator).images
-    vis = []
-    for img, mask, image in zip(imgs, masks, images):
-        vis += [img, mask, image, init_image, ImageChops.difference(img, image)]
-    make_image_grid(vis, rows=batch_size, cols=5).save(os.path.join(output, subject + ".jpg"))
+    img = np.array(init_image)
+    n_masks = np.random.randint(*ms)
+    mask = np.zeros(img.shape, dtype=np.uint8)
+    for i in range(n_masks):
+        h = np.random.randint(*ws)
+        w = np.random.randint(*hs)
+        x = np.random.randint(init_image.height // 2, init_image.height - h)
+        y = np.random.randint(0, init_image.width - w)
+        mask[x:x + h, y:y + w, :] = 255
+        img[x:x + h, y:y + w, :] = 0
+    mask = Image.fromarray(mask)
+    img = Image.fromarray(img)
+    image = pipeline(prompt=[prompt], image=[img], mask_image=[mask], height=shape[0], width=shape[1],
+                     generator=generator).images[0]
+    vis = [img, mask, image, init_image, ImageChops.difference(img, image)]
+    os.makedirs(os.path.join(output, subject), exist_ok=True)
+    image.save(os.path.join(output, subject, "material0.jpeg"))
+    make_image_grid(vis, rows=1, cols=5).save(os.path.join(output, subject + ".jpeg"))
--- inpaint-single.py
+++ inpaint-single.py
@@ -1,1 +1,22 @@
 # Created by Baole Fang at 1/23/24
+
+import torch
+from diffusers import AutoPipelineForInpainting
+from diffusers.utils import load_image
+
+pipeline = AutoPipelineForInpainting.from_pretrained(
+    "runwayml/stable-diffusion-inpainting",
+    torch_dtype=torch.float16,
+    variant="fp16",
+    safety_checker=None,
+    requires_safety_checker=False,
+)
+pipeline.enable_model_cpu_offload()
+
+generator = torch.Generator("cuda").manual_seed(92)
+prompt = "high-quality 2D uv texture map of a human body"
+
+img = load_image("aggregated_rgb.png")
+mask = load_image("inpaint_mask.png")
+image = pipeline(prompt=[prompt], image=[img], mask_image=[mask], width=img.width, height=img.height, generator=generator).images[0]
+image.save("inpaint.png")
